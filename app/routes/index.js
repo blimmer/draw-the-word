25d651467e0b2feb083ac4497d4cc8d21c76a71f
Ben Limmer <ben@benlimmer.com>
2016-04-17 18:51:49 -0600

    Pull nick from FB API.

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,4 +1,15 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model() {
+    return new Ember.RSVP.Promise((resolve, reject) => {
+      window.FB.api('/me', { fields: ['first_name', 'last_name']}, function(res) {
+        if (!res || res.error) {
+          reject(res.error);
+        } else {
+          resolve({nick: `${res.first_name} ${res.last_name.charAt(0)}`});
+        }
+      });
+    });
+  }
 });
